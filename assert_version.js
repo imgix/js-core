283b266d04c1f18f9593157e3d4c04f6e9d8ba02
kellysutton <michael.k.sutton@gmail.com>
2015-06-15 17:03:34 -0700

    Whoops

--- assert_version.js
+++ assert_version.js
@@ -2,8 +2,8 @@ var packageVersion = require('./package').version;
 var libVersion = require('./lib/imgix-core-js').VERSION;
 
 if (packageVersion === libVersion) {
-  process.stdout.write("FAIL: package.json and lib/imgix-core-js.js versions do not match!\n");
   return 0;
 } else {
+  process.stdout.write("FAIL: package.json and lib/imgix-core-js.js versions do not match!\n");
   return 1;
 }
