2103ffceeb58126362b58c7063d79e979e9beac4
kellysutton <michael.k.sutton@gmail.com>
2015-06-22 18:14:14 -0700

    Inlining lodash.merge to cut down on the total dependency size. This causes some bloat to imgix-core-js.umd.js, but it gzips down well.

--- src/imgix-core-js.js
+++ src/imgix-core-js.js
@@ -1,6 +1,6 @@
 import md5 from "js-md5";
 import URI from "URIjs";
-import _ from "lodash";
+import merge from "lodash/object/merge";
 
 export const VERSION = "0.2.0";
 
@@ -35,12 +35,12 @@ export class Path {
   }
 
   toUrl(newParams) {
-    this.queryParams = _.merge(this.queryParams, newParams);
+    this.queryParams = merge(this.queryParams, newParams);
     return this;
   }
 
   _query() {
-    return URI.buildQuery(_.merge(this._queryWithoutSignature(), this._signature()));
+    return URI.buildQuery(merge(this._queryWithoutSignature(), this._signature()));
   }
 
   _queryWithoutSignature() {
---
+++ src/lodash/lang.js
@@ -0,0 +1,21 @@
+import isArguments from './lang/isArguments';
+import isArray from './lang/isArray';
+import isFunction from './lang/isFunction';
+import isNative from './lang/isNative';
+import isObject from './lang/isObject';
+import isPlainObject from './lang/isPlainObject';
+import isString from './lang/isString';
+import isTypedArray from './lang/isTypedArray';
+import toPlainObject from './lang/toPlainObject';
+
+export default {
+  'isArguments': isArguments,
+  'isArray': isArray,
+  'isFunction': isFunction,
+  'isNative': isNative,
+  'isObject': isObject,
+  'isPlainObject': isPlainObject,
+  'isString': isString,
+  'isTypedArray': isTypedArray,
+  'toPlainObject': toPlainObject
+};
