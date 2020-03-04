f200ac811899f69cab7cd67a509ced6f02cbf9f2
Sherwin H <sherwin@imgix.com>
2020-03-03 17:07:58 -0800

    implement interface that defines all possible srcset option arguments

--- src/imgix-core-js.d.ts
+++ src/imgix-core-js.d.ts
@@ -8,13 +8,22 @@ declare class ImgixClient {
 
     constructor(opts: {domain: string; secureURLToken?: string; useHTTPS?: boolean; includeLibraryParam?: boolean;});
 
-
     buildURL(path: string, params?: {}): string;
     _sanitizePath(path: string): string;
     _buildParams(params: {}): string;
     _signParams(path: string, queryParams?: {}): string;
-    buildSrcSet(path: string, params?: {}, options?: {}): string;
-    _buildSrcSetPairs(path: string, params?: {}, options?: {}): string;
-    _buildDPRSrcSet(path: string, params?: {}, options?: {}): string;
+    buildSrcSet(path: string, params?: {}, options?: SrcSetOptions): string;
+    _buildSrcSetPairs(path: string, params?: {}, options?: SrcSetOptions): string;
+    _buildDPRSrcSet(path: string, params?: {}, options?: SrcSetOptions): string;
+
+}    
+
+interface SrcSetOptions {
+    widths?: number[];
+    widthTolerance?: number;
+    minWidth?: number;
+    maxWidth?: number;
+    disableVariableQuality?: boolean;
 }
+
 export = ImgixClient;
