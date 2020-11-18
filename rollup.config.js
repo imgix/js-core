f62e8db161739e079bbda067ecae898b5bc921fd
Eric Sanchez <esanchez@imgix.com>
2020-11-18 14:41:25 -0800

    feat: add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/main.js',
+    output: {
+      name: 'imgix',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  },
+  {
+    input: 'src/main.js',
+    external: ['md5', 'js-base64'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  }
+];
