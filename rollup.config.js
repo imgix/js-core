cf499860951bb7496ccfe74886af324a33425d2a
Eric Sanchez <esanchez@imgix.com>
2020-12-09 16:10:44 -0800

    feat: generalize test-suite transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -22,7 +22,7 @@ export default [
   },
   {
     input: 'src/main.js',
-    external: ['md5', 'js-base64'],
+    external: ['md5', 'js-base64', 'assert'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
@@ -33,47 +33,36 @@ export default [
       })
     ]
   },
-  {
-    input: 'test/test-buildSrcSet.js',
-    external: ['md5', 'js-base64'],
+  // Transpile test suite to cjs. This step allows us to write our
+  // tests with es6 module syntax while keeping our CI testing simple
+  // (as our CI environment expects to test in a node-based environment).
+  ...testSuiteHelper()
+];
+
+// testSuiteHelper maps cjsConfig over the lists of test-files.
+// The result is a list of configuration objects.
+function testSuiteHelper() {
+  const testFiles = [
+    'test-buildSrcSet',
+    'test-buildURL',
+    'test-client',
+    'test-validators'
+  ];
+
+  return testFiles.map(f => cjsConfig(f));
+}
+
+// cjsConfig accepts an extension-less file name and applies the
+// appropriate configuration information before returning the
+// configuration object.
+function cjsConfig(testFile) {
+  return {
+    input: `test/${testFile}.js`,
+    // Externalize dependencies so that they are excluded from the
+    // final bundle.
+    external: ['md5', 'js-base64', 'assert'],
     output: [
-      { file: 'test/build/test-buildSrcSet.cjs', format: 'cjs' },
-    ],
-    plugins: [
-      babel({
-        exclude: ['node_modules/**']
-      })
-    ]
-  },
-  {
-    input: 'test/test-buildURL.js',
-    external: ['md5', 'js-base64'],
-    output: [
-      { file: 'test/build/test-buildURL.cjs', format: 'cjs' },
-    ],
-    plugins: [
-      babel({
-        exclude: ['node_modules/**']
-      })
-    ]
-  },
-  {
-    input: 'test/test-client.js',
-    external: ['md5', 'js-base64'],
-    output: [
-      { file: 'test/build/test-client.cjs', format: 'cjs' },
-    ],
-    plugins: [
-      babel({
-        exclude: ['node_modules/**']
-      })
-    ]
-  },
-  {
-    input: 'test/test-validators.js',
-    external: ['md5', 'js-base64'],
-    output: [
-      { file: 'test/build/test-validators.cjs', format: 'cjs' },
+      { file: `test/build/${testFile}.cjs`, format: 'cjs' },
     ],
     plugins: [
       babel({
@@ -81,4 +70,4 @@ export default [
       })
     ]
   }
-];
+}
