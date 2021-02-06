7e66550e6e798db51cf9ffd8b151af5215e6fc89
Eric Sanchez <esanchez@imgix.com>
2021-02-05 16:37:10 -0800

    chore(release): v3.0.0-beta.1

--- src/constants.mjs
+++ src/constants.mjs
@@ -1,5 +1,5 @@
 // package version used in the ix-lib parameter
-export const VERSION = '2.3.2';
+export const VERSION = '3.0.0-beta.1';
 // regex pattern used to determine if a domain is valid
 export const DOMAIN_REGEX = /^(?:[a-z\d\-_]{1,62}\.){0,125}(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)[a-z\d]{1,63}$/i;
 // minimum generated srcset width
