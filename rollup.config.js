7303a3ed2c501d43e119f7b15271817e398c72c2
Eric Sanchez <esanchez@imgix.com>
2021-03-17 15:37:00 -0700

    build: remove browser attribute from package.json

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default [
     input: 'src/index.js',
     output: {
       name: 'ImgixClient',
-      file: pkg.browser,
+      file: "dist/imgix-js-core.umd.js",
       format: 'umd',
     },
     plugins: [
