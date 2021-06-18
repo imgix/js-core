30b908ff7efdba300f3d78c69897f69beeb3d0ca
Luis Ball <lball@imgix.com>
2021-06-17 17:05:50 -0700

    fix: handle proxy URLs in extractUrl
    Co-authored-by: Sherwin Heydarbeygi <sherwin@imgix.com>

--- src/helpers.js
+++ src/helpers.js
@@ -1,20 +1,92 @@
-export function extractUrl({ url, options }) {
-  let prefix, rest, domain, _path, path;
+/**
+ * `extractUrl()` extracts the domain and path from a URL string.
+ *
+ * - `url` can be a partial, full URL, or full proxy URL
+ * - `options` should be an object with `ImgixClient` options
+ *
+ * @returns {Object} `{domain, path}` (strings) extracted from the URL.
+ */
+export function extractUrl({ url = '', options = {} }) {
+  let domain = '';
+  let path = '';
 
-  const hasPrefix = url.indexOf('://') !== -1;
-  // store each component of the URL
-  if (!hasPrefix) {
-    prefix = options.useHTTPS ? 'https://' : 'http://';
-    [domain, ..._path] = url.split('/');
-  } else {
-    prefix = '';
-    rest = url.split('://')[1];
-    [domain, ..._path] = rest.split('/');
+  // base case
+  if (url == null || url.length <= 1) {
+    return { domain, path };
   }
-  path = _path.join('/');
+
+  // parse URL attributes
+  const hasScheme = url.indexOf('://') !== -1;
+  const isProxy = (url.match(/http/g) || []).length >= 2;
+  const useHttps = options.useHttps
+    ? options.useHttps
+    : url.indexOf('https') !== -1;
+  const scheme = useHttps ? 'https://' : 'http://';
+
+  if (isProxy) {
+    [domain, path] = extractProxyUrl({ url, scheme, useHttps });
+  } else if (!hasScheme) {
+    [domain, path] = extractDomainAndPath({ url });
+  } else {
+    [domain, path] = extractDomainAndPath({ url, scheme });
+  }
+
   return {
-    prefix,
     domain,
     path,
   };
 }
+
+/**
+ * `extractDomainAndPath()` first checks if the URL has a scheme. If it it does,
+ * it removes the scheme. Then it extracts the domain and path components of the
+ *  URL.
+ *
+ * - `url` string can be a partial or full URL, `https://foo.com` || `foo.com`
+ * - `scheme` (optional) string should be `http://` or `https://`.
+ *   Defaults to undefined.
+ *
+ * @returns Array of `[domain, path]` strings
+ */
+const extractDomainAndPath = ({ url, scheme = undefined }) => {
+  let _scheme, domain, _path, path;
+
+  // remove the scheme from the URL
+  if (scheme != null) {
+    [_scheme, url] = url.split(scheme);
+  }
+
+  [domain, ..._path] = url.split('/');
+  path = _path.join('/');
+  return [domain, path];
+};
+
+/**
+ *
+ * `extractProxyUrl()` extracts the URL's `domain` and proxy `path`. First, it
+ * removes the full URL's scheme from the string. Then it isolates the `domain`
+ * and `path` components fo the URL. Finally, it conditionally adds a `scheme`
+ * back to the `proxy` path, since it might have been removed when the full
+ * URL's scheme was split on.
+ *
+ * - `url` is a full URl with a proxy URL as its path
+ * - `scheme` string should be `http://` or `https://`
+ * - `useHttps` boolean defaults to `true`
+ *
+ * @returns Array of `[domain, path]` strings, where path is the proxy URL.
+ */
+const extractProxyUrl = ({ url, scheme, useHttps = true }) => {
+  let _scheme, domain, path;
+
+  // remove the scheme from the URL
+  [_scheme, domain, ...path] = url.split(scheme);
+  // remove any trailing slashes from the domain
+  domain = domain.replace(/\/$/, '');
+
+  // add scheme back in to the proxy path
+  if (path.indexOf('http') == -1) {
+    path = useHttps ? 'https://' + path : 'http://' + path;
+  }
+
+  return [domain, path];
+};
