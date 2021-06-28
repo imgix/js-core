46b0d6348c363e54f42de6186906925cd5570f1d
sherwinski <sherwin@imgix.com>
2021-06-28 13:36:00 -0700

    chore(release): v3.2.1

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -152,7 +152,7 @@ function _nonIterableRest() {
 }
 
 // package version used in the ix-lib parameter
-var VERSION = '3.2.0'; // regex pattern used to determine if a domain is valid
+var VERSION = '3.2.1'; // regex pattern used to determine if a domain is valid
 
 var DOMAIN_REGEX = /^(?:[a-z\d\-_]{1,62}\.){0,125}(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)[a-z\d]{1,63}$/i; // minimum generated srcset width
 
