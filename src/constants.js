1ca52f95d27f37a57985dc15bfc54cb182121d48
imgix-git-robot <upport@imgix.com>
2021-07-08 18:58:43 +0000

    chore(release): 3.3.0
    # [3.3.0](https://github.com/imgix/js-core/compare/v3.2.1...v3.3.0) (2021-07-08)
    
    ### Features
    
    * add srcsetModifiers and clientOptions params ([b0d6685](https://github.com/imgix/js-core/commit/b0d6685156f7566dd053c5a3048a60db1b59c94b))
    * create extractUrl helper ([5a5048e](https://github.com/imgix/js-core/commit/5a5048ed2889c9e2a4f397d62bebf0016f401bd2))
    * create static buildURL method ([9027a82](https://github.com/imgix/js-core/commit/9027a829ab87d81ef1434f139f23196e63c697ba))
    * static buildSrcSet method ([ea99366](https://github.com/imgix/js-core/commit/ea9936681a7b2f20d9c7c1992d731339871016d3))
    
     [skip ci]

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // package version used in the ix-lib parameter
-export const VERSION = '3.3.0-rc.1';
+export const VERSION = '3.3.0';
 // regex pattern used to determine if a domain is valid
 export const DOMAIN_REGEX = /^(?:[a-z\d\-_]{1,62}\.){0,125}(?:[a-z\d](?:\-(?=\-*[a-z\d])|[a-z]|\d){0,62}\.)[a-z\d]{1,63}$/i;
 // minimum generated srcset width
